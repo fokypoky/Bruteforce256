2f018dcc919937057cfd395c4026a931a5488cd1
Anton Petukhov <petuhova805@gmail.com>
2023-10-08 20:17:17 +0300

    Create ASP MVC UI

---
+++ WebBruteforce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBruteforce.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
